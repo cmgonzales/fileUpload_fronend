9f5aa35b7dc1fb87c1e61ec62efef3247bb87414
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-08-06 20:54:39 -0700

    changed config

---
+++ build/precache-manifest.1ad7f03de9c0756a65e882fa13bf01c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e354ff030a092747c4be8b691a4e1e20",
+    "url": "/static/media/photoscollage.e354ff03.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa69559144e420d5e376",
+    "url": "/static/js/main.c7948aec.chunk.js"
+  },
+  {
+    "revision": "60623e5ab54dc20a72a0",
+    "url": "/static/js/2.6b75fbff.chunk.js"
+  },
+  {
+    "revision": "fa69559144e420d5e376",
+    "url": "/static/css/main.831ac5f5.chunk.css"
+  },
+  {
+    "revision": "60623e5ab54dc20a72a0",
+    "url": "/static/css/2.154ec9be.chunk.css"
+  },
+  {
+    "revision": "f1605647fa9c79310038b497ae450137",
+    "url": "/index.html"
+  }
+];
