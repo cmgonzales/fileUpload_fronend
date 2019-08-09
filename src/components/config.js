2eff3a1bf23d3ed866fd284f294924d1d0ab14c7
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-08-08 22:02:29 -0700

    changed

--- src/components/config.js
+++ src/components/config.js
@@ -1,3 +1,3 @@
-exports.API_URL = process.env.NODE_ENV === 'production'
+export const API_URL = process.env.NODE_ENV === 'production'
   ? 'https://upload-serv.herokuapp.com/'
   : 'http://localhost:8080'
